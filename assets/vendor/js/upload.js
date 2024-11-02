68192552cd518ea42408d672e8b5ccb16ce287bb
My Name <91197408+hader-hussein@users.noreply.github.com>
2024-11-02 21:16:27 +0200

    2-11-2024

--- assets/vendor/js/upload.js
+++ assets/vendor/js/upload.js
@@ -51,49 +51,7 @@ $('.accordion-button').on('keydown',function (e) {
   
    
  })
- /***firstDropDownTree */
-// $("#dropdowntree").kendoDropDownTree({
-//    placeholder: "Select ...",
-//    filter: "startswith",
-//    dataSource: [
-//        {
-//            text: "Furniture", expanded: true, items: [
-//                { text: "Tables & Chairs" },
-//                { text: "Sofas" },
-//                { text: "Occasional Furniture" }
-//            ]
-//        },
-//        {
-//            text: "Decor", items: [
-//                { text: "Bed Linen" },
-//                { text: "Curtains & Blinds" },
-//                { text: "Carpets" }
-//            ]
-//        }
-//    ]
-// });
-/**tow */
-// $("#dropdowntree-tow").kendoDropDownTree({
-//    placeholder: "Select ...",
-//    filter: "startswith",
-//    dataSource: [
-//        {
-//            text: "Furniture", expanded: true, items: [
-//                { text: "Tables & Chairs" },
-//                { text: "Sofas" },
-//                { text: "Occasional Furniture" }
-//            ]
-//        },
-//        {
-//            text: "Decor", items: [
-//                { text: "Bed Linen" },
-//                { text: "Curtains & Blinds" },
-//                { text: "Carpets" }
-//            ]
-//        }
-//    ]
-// });
-
+ 
 
  // Function to handle the keyboard shortcut
  
