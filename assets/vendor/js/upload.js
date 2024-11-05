bc8482685e2d8e2d6414565f9b9e924c9357376f
My Name <91197408+hader-hussein@users.noreply.github.com>
2024-11-05 20:54:03 +0200

    11-5-2024

--- assets/vendor/js/upload.js
+++ assets/vendor/js/upload.js
@@ -27,23 +27,7 @@ $(function(){
     }, 8000);
 });
 
-$('.tabel-table').on('keydown','tr td',function (e) {
-    var keyCode = e.keyCode;
-    if (keyCode !== 9) return;
 
-    var $this = $(this),
-        $lastTr = $('tr:last', $('table')),
-        $lastTd = $('td:last', $lastTr);
-
-    
-    if (($(e.target).closest('td')).is($lastTd)) {
-     
-
-        $lastTr.after($lastTr.clone());
-      
-    }
-    
-});
 $('.accordion-button').on('keydown',function (e) {
  
  
